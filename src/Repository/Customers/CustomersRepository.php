52621b0f495ecd06ecb9753b6ccb9935a1937ef4
vgalitsky <concept.galitsky@gmail.com>
2026-06-14 12:10:47 -0400

    customer repository: basic search

--- src/Repository/Customers/CustomersRepository.php
+++ src/Repository/Customers/CustomersRepository.php
@@ -7,23 +7,73 @@ use Nette\Database\Table\Selection;
 
 class CustomersRepository implements CustomersRepositoryInterface
 {
-    const TABLE_NAME = 'customers';
-    const PAGE_SIZE = 20;
 
+    /**
+     * @param Explorer $explorer
+     */
     public function __construct(
         private Explorer $explorer,
     )
     {}
 
+    /**
+     * @inheritDoc
+     */
+    public function count(): int
+    {
+        return $this->select(['COUNT(id) AS total'])->fetch()->total;
+    }
+
+    /**
+     * @inheritDoc
+     */
     public function get(int $id): ?ActiveRow
     {
         return $this->select()->where('id = ?', $id)->fetch();
     }
 
-
-    private function select(array $columns = ['*']): Selection
+    /**
+     * @inheritDoc
+     */
+    public function byPublicId(string $publicId): ?ActiveRow
     {
-        return $this->explorer->table(self::TABLE_NAME)->select(...$columns);
+        return $this->select()->where('public_id = ?', $publicId)->fetch();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function search(
+        int $page, int $perPage = self::PAGE_SIZE
+    ): Selection
+    {
+        $page = max(1, $page);
+        $perPage = max(1, $perPage);
+        $offset = ($page - 1) * $perPage;
+
+        
+        $select = $this->select([
+                'customers.*',
+                'COUNT(:customer_activities.id) AS activities_count',
+            ])
+            ->group('id')
+            ->page($page, $perPage);
+
+        return $select;
+    }
+
+
+
+    /**
+     * @param array $columns
+     * @param mixed ...$params
+     * 
+     * @return Selection
+     */
+    private function select(array $columns = ['*'], mixed ...$params): Selection
+    {
+        $columns = join(', ', $columns);
+        return $this->explorer->table(self::TABLE_NAME)->select($columns, ...$params);
     }
 
 }
--- src/Repository/Customers/CustomersRepositoryInterface.php
+++ src/Repository/Customers/CustomersRepositoryInterface.php
@@ -1,8 +1,50 @@
 <?php
 namespace Mtr\MiniCRM\Repository\Customers;
 
+use Nette\Database\Table\ActiveRow;
+use Nette\Database\Table\Selection;
+
 interface CustomersRepositoryInterface
 {
-    public function get(int $id): ?\Nette\Database\Table\ActiveRow;
+    const TABLE_NAME = 'customers';
+    const PAGE_SIZE = 20;
+
+    /**
+     * Count total customers
+     * 
+     * @return int
+     */
+    public function count(): int;
+
+    /**
+     * Get customer by ID
+     * 
+     * @param int $id
+     * 
+     * @return ActiveRow|null
+     */
+    public function get(int $id): ?ActiveRow;
+
+    /**
+     * Get customer by public ID
+     * 
+     * @param string $publicId
+     * 
+     * @return ActiveRow|null
+     */
+    public function byPublicId(string $publicId): ?ActiveRow;
+
+    /**
+     * search customers
+     * 
+     * @param int $page
+     * @param int $perPage
+     * 
+     * @return \Nette\Database\Table\Selection
+     */
+    public function search(
+        int $page, int $perPage = self::PAGE_SIZE
+    ): Selection;
+
 
 }
